ebe935b3955298a698f1020e548570298e6e21ea
Barry Morrison <689591+esacteksab@users.noreply.github.com>
2020-09-02 14:46:59 -0500

    Add AWS Partition

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "main" {
       "logs:PutLogEvents",
     ]
 
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${local.name}-${var.environment}:*"]
+    resources = ["arn:${data.aws_partition.current.partition}:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${local.name}-${var.environment}:*"]
   }
 
   # Allow access to support APIs. Unfortunately, IAM doesn't provide
